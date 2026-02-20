24242f5745dcb1242a053a2b0d61df1c3fa7358c
JigneshAmmineni <jigneshammineni@gmail.com>
2026-02-20 13:08:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key='AIzaSyCPtbtlUsZwvb0Jqv-rGCCFt0f3QxccJgw')
-
+client = genai.Client(api_key='')
 response = client.models.generate_content(
     model="gemini-3-flash-preview",
     contents="How does AI work?"
